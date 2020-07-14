0cb2388c403c6455084ebe534153888003ce8309
1 <dgc0912376@gmail.com>
2020-07-14 17:24:35 +0800

    change background color

---
+++ Homework001/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework001
+//
+//  Created by 徐廷澤 on 2020/7/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Homework001Tests/Homework001Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Homework001Tests.swift
+//  Homework001Tests
+//
+//  Created by 徐廷澤 on 2020/7/14.
+//
+
+import XCTest
+@testable import Homework001
+
+class Homework001Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
